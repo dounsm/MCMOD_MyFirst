6625cff333f0715e1d3560a61f656ef93ae64a29
NewOS <13276545119@phone.com>
2023-01-04 22:27:36 +0800

    hand

--- src/main/java/com/dounsm/dounsmmod/blocks/TouZi.java
+++ src/main/java/com/dounsm/dounsmmod/blocks/TouZi.java
@@ -5,15 +5,20 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.ExplosionDamageCalculator;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.TntBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+
 
 public class TouZi extends Block {
     public static String m_block_id = "touzi";
@@ -26,7 +31,9 @@ public class TouZi extends Block {
 //        return super.use(blockState,level,blockPos,player,interactionHand,blockHitResult);
         if(blockHitResult.getDirection()!= Direction.UP){return super.use(blockState,level,blockPos,player,interactionHand,blockHitResult);};
 //        if(player.getHandSlots() TntBlock.)
-        level.explode(null,blockPos.getX(),blockPos.getY(),blockPos.getZ(),10.0f,Explosion.BlockInteraction.DESTROY);
+        if(player.getItemInHand(interactionHand).getItem().equals(Items.TNT) ) {
+            level.explode(null, blockPos.getX(), blockPos.getY(), blockPos.getZ(), 10.0f, Explosion.BlockInteraction.DESTROY);
+        };
         return InteractionResult.SUCCESS;
     }
 }
